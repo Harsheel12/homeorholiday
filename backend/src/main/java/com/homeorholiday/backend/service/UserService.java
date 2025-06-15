7da1c1b432bb45f300b56df466903ba627304891
Harsheel Singh <hsin212@aucklanduni.ac.nz>
2025-06-15 21:37:47 +1200

    feat: Created Initial UserService

---
+++ backend/src/main/java/com/homeorholiday/backend/service/UserService.java
@@ -0,0 +1,28 @@
+package com.homeorholiday.backend.service;
+
+import com.homeorholiday.backend.models.User;
+import com.homeorholiday.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User createUser(String name) {
+        User user = new User(name);
+        return userRepository.save(user);
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+}
