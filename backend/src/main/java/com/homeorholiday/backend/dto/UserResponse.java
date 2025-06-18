01ffc4c61b90edc8df3333d1a1f555f1853dbdc6
Harsheel Singh <hsin212@aucklanduni.ac.nz>
2025-06-19 00:13:00 +1200

    feat: Created a User response DTO

---
+++ backend/src/main/java/com/homeorholiday/backend/dto/UserResponse.java
@@ -0,0 +1,85 @@
+package com.homeorholiday.backend.dto;
+
+import com.homeorholiday.backend.model.User;
+
+import java.time.LocalDateTime;
+
+public class UserResponse {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String fullName;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public UserResponse() {
+
+    }
+
+    public UserResponse(User user) {
+        this.id = user.getId();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.email = user.getEmail();
+        this.fullName = user.getFullName();
+        this.createdAt = user.getCreatedAt();
+        this.updatedAt = user.getUpdatedAt();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
